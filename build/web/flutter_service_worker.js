d64b94028b052af2413cd254154e662e3487630d
dannyyys <dannyyys@users.noreply.github.com>
2021-05-08 15:44:24 +0800

    test

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1a4ecea448fb4fc4c6415f07bf21831d",
-"/": "1a4ecea448fb4fc4c6415f07bf21831d",
+"index.html": "3a610d213a75b3bfe3da8f2a22645cf5",
+"/": "3a610d213a75b3bfe3da8f2a22645cf5",
 "main.dart.js": "71989952cbaa9ae18df9a67eec716e86",
 "manifest.json": "6087c069b7dfb03b97e03caac9c47d84",
 "version.json": "247ea2d97c7da15a983ccf00157d2341"
